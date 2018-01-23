285559d8bfc1f08091bbf5ee08265f793f205561
Kayla Webb <kaylarwebb@gmail.com>
2018-01-23 12:07:01 -0500

    Include big O explanation for fibonacci sequence

--- big-o-drills.js
+++ big-o-drills.js
@@ -81,3 +81,34 @@ function createPairs(arr) {
 // function takes an array and produces all possible pairs of elements within array (order does not matter)
 // by virtue of there being one loop within another loop (one nested loop)
 // this algorithm has a polynomial time complexity. O(n^2).
+
+//=== GENERATE FIBONACCI ===//
+
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i === 2) {
+      result.push(1);
+    }
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+//depends on the number you pass to it
+//the function has one for loop and its run time will depend on how high the input value is
+//thereforce, the big O classification is linear - O(n)
